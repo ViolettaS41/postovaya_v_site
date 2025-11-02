0e9e9acb9eb93c5f640d3e85bdfab663099927c9
Violetta <165622447+ViolettaS41@users.noreply.github.com>
2025-11-02 22:13:37 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// EmailJS конфигурация
-// ВАЖНО: Этот файл содержит секретные ключи и не должен быть закоммичен в git!
-window.EMAILJS_CONFIG = {
-  publicKey: "GS36Pl4EX1KGiF9ZN",
-  serviceId: "service_1hwjpb4",
-  templateId: "template_s6wwn6l",
-};
